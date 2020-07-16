9a7644c715349aa1a3b6a57db36ae5f90354d975
AlexandreCR <devalexandrecr@gmail.com>
2020-07-16 11:55:58 -0500

    feat: add request photo and rules on request products

---
+++ app/Http/Requests/Products/StoreRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Products;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['string', 'max:100', 'min:3', 'required'],
+            'description' => ['string', 'min:50', 'max:300'],
+            'stock' => ['integer'],
+            'price' => ['string', 'required|regex:/^\d*(\.\d{1.2})?$/'],
+            'id_category' => ['integer'],
+            'is_active' => ['boolean']
+        ];
+    }
+}
