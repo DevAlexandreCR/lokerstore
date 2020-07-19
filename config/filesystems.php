45602e405f62a1e8f6d3eb3b3ce8722bfe1781f8
AlexandreCR <devalexandrecr@gmail.com>
2020-07-18 21:05:25 -0500

    fix: route images to link storage images

--- config/filesystems.php
+++ config/filesystems.php
@@ -88,6 +88,7 @@ return [
     'links' => [
         public_path('storage') => storage_path('app/public'),
         public_path('photos') => storage_path('app/public/photos'),
+        public_path('images') => storage_path('app/public/images'),
     ],
 
 ];
