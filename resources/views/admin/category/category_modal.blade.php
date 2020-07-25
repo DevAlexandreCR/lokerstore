c599f7e180903d7370b65748923f3ffc0bd44357
AlexandreCR <devalexandrecr@gmail.com>
2020-07-24 19:11:47 -0500

    feat: traslate text categories

--- resources/views/admin/category/category_modal.blade.php
+++ resources/views/admin/category/category_modal.blade.php
@@ -4,7 +4,7 @@
         <form action="{{route('category.store')}}" method="post">
             @csrf
             <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">{{__('Add Category')}}</h5>
+            <h5 class="modal-title" id="exampleModalLabel">{{__('Add category')}}</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
@@ -14,11 +14,11 @@
                     <input type="number" hidden name="id_parent" value="{{null}}">
                     <label for="exampleInputEmail1">{{__('Name')}}</label>
                     <input type="text" class="form-control" id="inputName" name="name" aria-describedby="namelHelp" value="{{ old('name') }}">
-                    <small id="nameHelp" class="form-text text-muted">{{__('The name most be unique')}}</small>
+                    <small id="nameHelp" class="form-text text-muted">{{__('The name must be unique')}}</small>
                   </div>
             </div>
             <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">{{__('Cancel')}}</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">{{__('Discard')}}</button>
             <button type="submit" class="btn btn-primary">{{__('Save')}}</button>
             </div>
         </form>
