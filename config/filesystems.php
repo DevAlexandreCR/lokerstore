e079f63b42c77ec74b64378140a3437072532f6e
AlexandreCR <devalexandrecr@gmail.com>
2020-08-21 12:35:12 -0500

    fix: refactor test photos

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,13 @@ return [
             'visibility' => 'public',
         ],
 
+        'public_photos' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/photos'),
+            'url' => env('APP_URL').'/photos',
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
