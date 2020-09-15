b4f5e51098fe1cf2e20d2a1221f0c5d503d45156
AlexandreCR <devalexandrecr@gmail.com>
2020-09-14 20:28:14 -0500

    fix: remove policies files

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,10 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\Order;
-use App\Models\User;
-use App\Policies\OrderPolicy;
-use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -15,17 +11,14 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @var array
      */
-    protected $policies = [
-         User::class => UserPolicy::class,
-        Order::class => OrderPolicy::class
-    ];
+    protected $policies = [];
 
     /**
      * Register any authentication / authorization services.
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         $this->registerPolicies();
     }
