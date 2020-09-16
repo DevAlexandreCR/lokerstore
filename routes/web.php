dd6da18f1750021eecc5c6c9281ea6507e8b6f45
AlexandreCR <devalexandrecr@gmail.com>
2020-09-16 13:06:23 -0500

    feat: add artisan command ti route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Auth::routes(['verify' => true]);
+
+// add route to run command to links heroku
+Route::get('/artisan/storage', function() {
+    $command = 'storage:link';
+    Artisan::call($command);
+    return Artisan::output();
+});
 
 // los usuarios inhabilitados no pueden acceder al index
 Route::get('/', 'HomeController@index')
