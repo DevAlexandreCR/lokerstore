c373652805f650821a68d348149e3221780c86b1
AlexandreCR <devalexandrecr@gmail.com>
2020-11-25 19:30:10 -0500

    remove compiled files from public folder

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,7 +42,7 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    protected function credentials(Request $request)
+    protected function credentials(Request $request): array
     {
         $credenctials = $request->only($this->username(), 'password');
 
@@ -75,7 +75,7 @@ class LoginController extends Controller
     /**
      * @return Guard
      */
-    protected function guard()
+    protected function guard(): Guard
     {
         return Auth::guard();
     }
--- database/seeds/DummyDatabaseSeeder.php
+++ database/seeds/DummyDatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
-    use App\Constants\Orders;
-    use Illuminate\Database\Seeder;
+use App\Constants\Orders;
+use Illuminate\Database\Seeder;
 
 class DummyDatabaseSeeder extends Seeder
 {
@@ -23,10 +23,19 @@ class DummyDatabaseSeeder extends Seeder
             ]
         );
         \App\Models\Order::all()->each(function ($order) {
-            $order->status = $this->makeFaker(config('app.locale'))->randomElement([
-                Orders::STATUS_CANCELED,
-                Orders::STATUS_SUCCESS,
-            ]);
+            if (now()->subMonth() < $order->created_at) {
+                $order->status = $this->makeFaker(config('app.locale'))->randomElement([
+                    Orders::STATUS_PENDING_PAY,
+                    Orders::STATUS_SUCCESS,
+                    Orders::STATUS_SENT,
+                    Orders::STATUS_PENDING_SHIPMENT,
+                ]);
+            } else {
+                $order->status = $this->makeFaker(config('app.locale'))->randomElement([
+                    Orders::STATUS_CANCELED,
+                    Orders::STATUS_SUCCESS,
+                ]);
+            }
             $order->save();
         });
     }
